2cc9fd76da636914677e4abac43ed8930a8f4d03
Leaf Ying <leaf.ying.work@gmail.com>
2024-08-06 21:38:54 +0800

    Improve image loading and pre-processing step with a general way

--- inference.py
+++ inference.py
@@ -8,7 +8,7 @@
 # OF ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
-import itertools
+import mimetypes
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from pathlib import Path
 from typing import List, Optional, Sequence, Tuple, Union
@@ -32,14 +32,14 @@ def get_args():
         "fg", type=str, help="Directory or Path to the foreground image(s)."
     )
     parser.add_argument(
-        "--fg_mask",
+        "--fg-mask",
         type=str,
         default=None,
         help="Directory or Path to the foreground mask image(s).",
     )
 
     parser.add_argument(
-        "--out_dir",
+        "--out-dir",
         type=str,
         default="results",
         help="Path to export results.",
@@ -84,6 +84,9 @@ def resize_and_pad(
 ) -> Image.Image:
     target_w, target_h = size
     w, h = img.size
+
+    if img.size == size:
+        return img
 
     # resize the longest edge
     target_ratio = target_w / target_h
@@ -151,48 +154,48 @@ class Inferencer:
         if fg_mask_paths is None:
             fg_mask_paths = [None] * len(fg_paths)
 
-        for fg_path, fg_mask_path in zip(fg_paths, fg_mask_paths):
+        for bg_path, fg_path, fg_mask_path in zip(bg_paths, fg_paths, fg_mask_paths):
+            bg_path = Path(bg_path)
+            bg_img = Image.open(bg_path).convert("RGB")
+
             fg_path = Path(fg_path)
             fg_img = Image.open(fg_path)
+
             if fg_mask_path is None:
                 assert fg_img.mode == "RGBA"
                 fg_mask = fg_img.getchannel("A")
             else:
-                fg_mask = Image.open(fg_mask_path).convert("1")
-            fg_img = fg_img.convert("RGB")
+                fg_mask = Image.open(fg_mask_path)
+                assert fg_mask.mode == "L"
 
-            for bg_path in bg_paths:
-                bg_path = Path(bg_path)
-                bg_img = Image.open(bg_path).convert("RGB")
+            resized_fg_img = resize_and_pad(fg_img, bg_img.size)
+            resized_fg_mask = resize_and_pad(fg_mask, bg_img.size)
+            composite_img = Image.composite(resized_fg_img, bg_img, resized_fg_mask)
 
-                resized_fg_img = resize_and_pad(fg_img, bg_img.size)
-                resized_fg_mask = resize_and_pad(fg_mask, bg_img.size)
-                composite_img = Image.composite(resized_fg_img, bg_img, resized_fg_mask)
+            bg_low = self.resize(bg_img)
+            composite_low = self.resize(composite_img)
+            mask_low = self.resize(resized_fg_mask)
 
-                bg_low = self.resize(bg_img)
-                composite_low = self.resize(composite_img)
-                mask_low = self.resize(resized_fg_mask)
+            torch_bg = self.transform(bg_img)
+            torch_composite = self.transform(composite_img)
+            torch_mask = self.transforms_mask(resized_fg_mask)
 
-                torch_bg = self.transform(bg_img)
-                torch_composite = self.transform(composite_img)
-                torch_mask = self.transforms_mask(resized_fg_mask)
+            torch_bg_low = self.transform(bg_low)
+            torch_composite_low = self.transform(composite_low)
+            torch_mask_low = self.transforms_mask(mask_low)
 
-                torch_bg_low = self.transform(bg_low)
-                torch_composite_low = self.transform(composite_low)
-                torch_mask_low = self.transforms_mask(mask_low)
-
-                yield (
-                    bg_path,
-                    fg_path,
-                    composite_img,
-                    resized_fg_mask,
-                    torch_bg,
-                    torch_composite,
-                    torch_mask,
-                    torch_bg_low,
-                    torch_composite_low,
-                    torch_mask_low,
-                )
+            yield (
+                bg_path,
+                fg_path,
+                composite_img,
+                resized_fg_mask,
+                torch_bg,
+                torch_composite,
+                torch_mask,
+                torch_bg_low,
+                torch_composite_low,
+                torch_mask_low,
+            )
 
     def to(self, inputs: Sequence, device: str):
         return [
@@ -239,11 +242,15 @@ class Inferencer:
         results: torch.Tensor,
         gainmap: torch.Tensor,
         curves: torch.Tensor,
-        out_dir: Path = Path("results"),
+        out_dir: Union[str, Path] = "results",
+        fg_input_dir: Optional[Union[str, Path]] = None,
     ):
+        out_dir = Path(out_dir)
+        if fg_input_dir is not None:
+            out_dir = out_dir / fg_path.relative_to(fg_input_dir).parent
         out_dir.mkdir(parents=True, exist_ok=True)
 
-        name_cat = f"{fg_path.stem}_{bg_path.stem}"
+        name_cat = fg_path.stem
 
         output_lr = F.to_pil_image(results[0])
         output_lr.save(out_dir / f"{name_cat}_final.png")
@@ -280,38 +287,93 @@ class Inferencer:
         bg_paths: List[Union[str, Path]],
         fg_paths: List[Union[str, Path]],
         fg_mask_paths: Optional[List[Union[str, Path]]] = None,
-        out_dir: Path = Path("results"),
+        out_dir: Union[str, Path] = "results",
+        fg_input_dir: Optional[Union[str, Path]] = None,
     ):
+        assert len(bg_paths) == len(
+            fg_paths
+        ), "The number of background and foreground images should be the same."
+        assert fg_mask_paths is None or len(fg_paths) == len(
+            fg_mask_paths
+        ), "The number of foreground and foreground mask images should be the same."
+
         inputs = self.preprocess(bg_paths, fg_paths, fg_mask_paths)
-        for data in tqdm(inputs, desc="Processing..."):
+        for data in tqdm(inputs, desc="Processing...", total=len(fg_paths)):
             data = self.to(data, self.device)
             ouputs = data[:4] + list(self.forward(*data[4:]))
-            self.visualize(*ouputs, out_dir=out_dir)
+            self.visualize(*ouputs, out_dir=out_dir, fg_input_dir=fg_input_dir)
 
 
-def collect_images(
-    path: Optional[str],
-):
-    if path is None:
-        return None
+def collect_images(path: Union[str, Path]) -> List[Path]:
+    mime_checker = mimetypes.MimeTypes()
 
-    root_path = Path(path)
-    if root_path.is_dir():
-        paths = sorted(
-            itertools.chain(root_path.rglob("*.jpg"), root_path.rglob("*.png"))
-        )
+    def validate_file_type(path: Path):
+        mime_type = mime_checker.guess_type(path)[0]
+        return mime_type is not None and mime_type.startswith("image")
+
+    path = Path(path)
+    if path.is_dir():
+        return sorted(filter(validate_file_type, path.rglob("*.*")))
     else:
-        paths = [root_path]
+        return [path]
 
+
+def collect_images_from_images(
+    images: List[Union[str, Path]],
+    root_path: Union[str, Path],
+    target_path: Union[str, Path],
+) -> List[Path]:
+    target_path = Path(target_path)
+    if target_path.is_file():
+        return [target_path]
+
+    paths = []
+    exts = [
+        ext
+        for ext, mime_type in mimetypes.types_map.items()
+        if mime_type.startswith("image")
+    ]
+    for image in images:
+        image = Path(image)
+        rel_path = image.relative_to(root_path)
+
+        for ext in exts:
+            path = target_path / rel_path.with_suffix(ext)
+            if path.exists():
+                break
+        else:
+            assert (
+                False
+            ), f"The corresponding background image is not found, {rel_path}."
+
+        paths.append(path)
     return paths
 
 
 if __name__ == "__main__":
     args = get_args()
-    bg_paths = collect_images(args.pop("bg"))
-    fg_paths = collect_images(args.pop("fg"))
-    fg_mask_paths = collect_images(args.pop("fg_mask"))
-    out_dir = Path(args.pop("out_dir"))
+
+    # collect foreground images
+    fg_root_path = args.pop("fg")
+    fg_paths = collect_images(fg_root_path)
+
+    # collect foreground mask images if exists
+    fg_mask_paths = args.pop("fg_mask")
+    if fg_mask_paths is not None:
+        fg_mask_paths = collect_images_from_images(
+            fg_paths, fg_root_path, fg_mask_paths
+        )
+
+    # collect background images
+    bg_paths = collect_images_from_images(fg_paths, fg_root_path, args.pop("bg"))
+
+    out_dir = args.pop("out_dir")
 
     evaluater = Inferencer(**args)
-    evaluater(bg_paths, fg_paths, fg_mask_paths=fg_mask_paths, out_dir=out_dir)
+    evaluater(
+        bg_paths,
+        fg_paths,
+        fg_mask_paths=fg_mask_paths,
+        out_dir=out_dir,
+        fg_input_dir=fg_root_path,
+    )
